c2e88a3ac6058168797c01283b86e3cdde39bcaf
svm92 <svm92@hotmail.es>
2018-11-08 17:41:35 +0100

    Solved navbar loading issues

---
+++ loadNavbar.js
@@ -0,0 +1,37 @@
+// Get document page language
+var language = document.documentElement.lang;
+
+// Load navigation bar corresponding to page's language
+if (language === "en") {
+    $("#navbar").load("navbar.html");
+}
+else if (language === "es") {
+    $("#navbar").load("es/navbar.html");
+}
+
+// Get current id of the page
+var navItemId = $("#page-index").text();
+
+// After the navbar loads, highlight the item with the current id (section of the website where the user is)
+var navItemExists = setInterval(function () {
+    if ($("#navbar").find(".nav-item").eq(navItemId).length) {
+        $("#navbar").find(".nav-item").eq(navItemId).addClass(" active");
+        clearInterval(navItemExists);
+    }
+}, 50);
+
+var pageName = location.href.split("/").slice(-1); 
+
+// After the navbar loads, change destination of dropdown flag menu to opposite language page
+var dropdownExists = setInterval(function () {
+    if ($("#navbar").find(".dropdown-menu").length) {
+        
+        if (language === "en") {
+            $("#navbar").find(".dropdown-menu").find("a").eq(0).attr("href", "es/" + pageName);
+        } else if (language === "es") {
+            $("#navbar").find(".dropdown-menu").find("a").eq(0).attr("href", pageName);
+        }
+        
+        clearInterval(dropdownExists);
+    }
+}, 50);
